4dc11e82b61a90d4778f2a87643380b3ea3b88c8
Leonardo <leonardo.fc2@outlook.com>
2017-12-20 23:16:03 -0200

    Programaçao OO

---
+++ 3 semestre/Programação OO/PrimeiraEquacao/src/primeiroPrograma/Principal.java
@@ -0,0 +1,8 @@
+package primeiroPrograma;                    
+                                             
+public class Principal {                      
+                                             
+    public static void main(String[] args) {  
+            new Janela();                    
+        }                                    
+}                 
