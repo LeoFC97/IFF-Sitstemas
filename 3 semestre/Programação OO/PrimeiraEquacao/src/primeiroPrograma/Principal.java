e6d4674aa96f2782fcbc7f8f7bcaa64834b38dec
Leonardo <leonardo.fc2@outlook.com>
2018-03-07 23:32:23 -0300

    teste

--- 3 semestre/Programação OO/PrimeiraEquacao/src/primeiroPrograma/Principal.java
+++
@@ -1,8 +0,0 @@
-package primeiroPrograma;                    
-                                             
-public class Principal {                      
-                                             
-    public static void main(String[] args) {  
-            new Janela();                    
-        }                                    
-}                 
--- 3 semestre/Programação OO/Trabalho_Java_1_2.0/src/Main.java
+++
@@ -1,9 +0,0 @@
-import Interface.Janela_Principal;
-
-public class Main {
-
-	public static void main(String[] args)
-	{
-		new Janela_Principal();
-	}
-}
